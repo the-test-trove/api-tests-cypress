c04d79a2037d016f33be8db8f97453f78d4435e9
veepatil <veepatil@redhat.com>
2022-05-31 17:09:56 +0530

    Initial Commit for API Testing

---
+++ cypress/support/commands.js
@@ -0,0 +1,74 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+
+
+Cypress.Commands.add('GETALL', (baseURL, activities) =>
+{
+   
+        cy.request(
+            {
+                method: "GET",
+                url : baseURL +"/api/v1/"+ activities ,
+                failOnStatusCode: false,
+                //Authtication if required
+                // auth:
+                // {
+                //     username : userName,
+                //     password: "password"
+                // },
+                headers:
+                {
+                    'Content-Type': 'application/json;charset=UTF-8'
+                },
+
+            })
+
+});
+
+
+Cypress.Commands.add('GETBYID', (baseURL, activities, id) =>
+{
+   
+        cy.request(
+            {
+                method: "GET",
+                url : baseURL +"/api/v1/"+ activities+"/"+id,
+                failOnStatusCode: false,
+                //Authtication if required
+                // auth:
+                // {
+                //     username : userName,
+                //     password: "password"
+                // },
+                headers:
+                {
+                    'Content-Type': 'application/json;charset=UTF-8'
+                },
+
+            })
+
+});
