1e26501545bcf9cf24cb4c84d7aa3ae659804892
veepatil <veepatil@redhat.com>
2022-06-14 17:48:02 +0530

    Added one failed test case

--- cypress/integration/API/Create_Activity_spec.js
+++ cypress/integration/API/Create_Activity_spec.js
@@ -15,5 +15,19 @@ describe('POST API Test', () => {
         
     });
 
+    it('Create Activity failed test', () => {
+
+        cy.fixture('activity.json').as('inputRequest')
+        cy.POST(Cypress.config().baseUrl,"Activities").then((response => 
+        {
+            expect(response.status,404)
+            cy.log("The Status cod receieved is: " + response.status)
+            cy.log( JSON.stringify(response.body))
+            }
+            
+            ))
+        
+    });
+
 
 });
