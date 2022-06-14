6c577c05f9e9715163baeadf0a30e856ef765052
veepatil <veepatil@redhat.com>
2022-06-14 19:07:04 +0530

    updated test case to pass

--- cypress/integration/API/Create_Activity_spec.js
+++ cypress/integration/API/Create_Activity_spec.js
@@ -20,7 +20,7 @@ describe('POST API Test', () => {
         cy.fixture('activity.json').as('inputRequest')
         cy.POST(Cypress.config().baseUrl,"Activities").then((response => 
         {
-            expect(response.status).to.equal(404)
+            expect(response.status).to.equal(200)
             cy.log("The Status cod receieved is: " + response.status)
             cy.log( JSON.stringify(response.body))
             }
